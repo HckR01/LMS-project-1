89c8a262e5f638cb9ff503e2781c9ae7dbc678da
HckR01 <139985239+HckR01@users.noreply.github.com>
2025-08-15 16:35:51 +0530

    speed tracker add in main.js

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 // import { StrictMode } from 'react';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { AppContextProvider } from './context/AppContext.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { ClerkProvider } from '@clerk/clerk-react'; //importinf clerk
+
+injectSpeedInsights();
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
