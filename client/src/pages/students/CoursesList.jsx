5c53aa67f37c242805d58d74e29adb3a0417c228
HckR01 <139985239+HckR01@users.noreply.github.com>
2025-08-18 12:06:24 +0530

    add the filter function in the course list

--- client/src/pages/students/CoursesList.jsx
+++ client/src/pages/students/CoursesList.jsx
@@ -1,11 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AppContext } from '../../context/AppContext';
 import SearchBar from '../../components/students/SearchBar';
+import CourseCard from '../../components/students/CourceCard';
 import { useParams } from 'react-router-dom';
 
 const CoursesList = () => {
   const { navigate, allCourses } = useContext(AppContext);
   const { input } = useParams();
+
+  // filter for searching
+  const [filteredCourses, setFilteredCourses] = useState([]);
+  useEffect(() => {
+    if (allCourses && allCourses.length > 0) {
+      const tempCourses = allCourses.slice();
+      input
+        ? setFilteredCourses(
+            tempCourses.filter((item) =>
+              item.courseTitle.toLowerCase().includes(input.toLocaleLowerCase())
+            )
+          )
+        : setFilteredCourses(tempCourses);
+    }
+  }, [allCourses, input]);
 
   return (
     <div className="position-relative md:px-36 px-8 pt-20 text-left">
@@ -26,6 +42,11 @@ const CoursesList = () => {
         </div>
         <SearchBar data={input} />
       </div>
+      <di className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-14">
+        {filteredCourses.map((course, index) => (
+          <CourseCard key={index} course={course} />
+        ))}
+      </di>
     </div>
   );
 };
