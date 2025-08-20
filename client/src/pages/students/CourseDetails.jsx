8f168037befb2b68358ceb42a5d6ee7d61ff3c06
HckR01 <139985239+HckR01@users.noreply.github.com>
2025-08-20 19:25:49 +0530

    add left part of the course-details

--- client/src/pages/students/CourseDetails.jsx
+++ client/src/pages/students/CourseDetails.jsx
@@ -1,10 +1,79 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { AppContext } from '../../context/AppContext';
+import Loading from '../../components/students/Loading';
+import { assets } from '../../assets/assets';
 
 const CourseDetails = () => {
-  return (
-    <div>
-      <h1>CourseDetails</h1>
-    </div>
+  const { id } = useParams();
+  const [courseData, setCourseData] = useState(null);
+  const { allCourses, calculateRating } = useContext(AppContext);
+
+  useEffect(() => {
+    const fetchCourseData = () => {
+      const findCourse = allCourses?.find((course) => course._id === id);
+      setCourseData(findCourse);
+    };
+    fetchCourseData();
+  }, []);
+
+  return courseData ? (
+    <>
+      <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
+        <div className="absolute top-0 left-0 w-full h-500  bg-gradient-to-b from-cyan-100/70"></div>
+        {/* left colunm////////////// */}
+        <div className="max-w-xl z-10 text-gray-500">
+          <h1 className="text-xl sm:text-2xl md:text-4xl lg:text-4xl font-semibold text-gray-800">
+            {courseData.courseTitle}
+          </h1>
+
+          <p
+            className="pt-4 md:text-base text-sm"
+            dangerouslySetInnerHTML={{
+              __html: courseData.courseDescription.slice(0, 200),
+            }}
+          ></p>
+          {/* course reviews */}
+          <div className="flex items-center space-x-2 pt-3 pb-1 text-sm">
+            <p>{calculateRating(courseData)}</p>
+            <div className="flex">
+              {[...Array(5)].map((_, i) => (
+                <img
+                  key={i}
+                  src={
+                    i < Math.floor(calculateRating(courseData))
+                      ? assets.star
+                      : assets.star_blank
+                  }
+                  alt=""
+                  className="w-3.5 h-3.5"
+                />
+              ))}
+            </div>
+            <p className="text-blue-600">
+              ({courseData.courseRatings.length}
+              {courseData.courseRatings.length > 1 ? ' Reviews' : ' Review'})
+            </p>
+            {/* total number of the student enrolled */}
+            <p>
+              {courseData.enrolledStudents.length}{' '}
+              {courseData.enrolledStudents.length > 1
+                ? ' Students'
+                : ' Student'}
+            </p>
+          </div>
+
+          <p className="text-sm">
+            Course by{' '}
+            <span className="text-blue-600 underline"> Greaatstack</span>
+          </p>
+        </div>
+        {/* right column//////////////// */}
+        <div></div>
+      </div>
+    </>
+  ) : (
+    <Loading />
   );
 };
 
--- client/src/pages/students/CoursesList.jsx
+++ client/src/pages/students/CoursesList.jsx
@@ -4,6 +4,7 @@ import SearchBar from '../../components/students/SearchBar';
 import CourseCard from '../../components/students/CourceCard';
 import { useParams } from 'react-router-dom';
 import { assets } from '../../assets/assets';
+import Footer from '../../components/students/Footer';
 
 const CoursesList = () => {
   const { navigate, allCourses } = useContext(AppContext);
@@ -25,44 +26,47 @@ const CoursesList = () => {
   }, [allCourses, input]);
 
   return (
-    <div className="position-relative md:px-36 px-8 pt-20 text-left">
-      <div className="flex items-start md:flex-row flex-col w-full justify-between">
-        <div>
-          <h1 className="text-4xl font-semibold text text-gray-800">
-            CoursesList
-          </h1>
-          <p className="text-gray-500">
-            <span
-              className="text-blue-600 cursor-pointer"
-              onClick={() => navigate('/')}
-            >
-              Home
-            </span>{' '}
-            / <span>CoursesList</span>
-          </p>
-        </div>
-        <SearchBar data={input} />
-      </div>
-      {
-        // conditionally render filter text
-        input && (
-          <div className="inline-flex items-center gap-4 px-4 py-2 bg-gray-100 rounded-full">
-            <p>{input}</p>
-            <img
-              src={assets.cross_icon}
-              alt="cursor"
-              className="cursor-pointer "
-              onClick={() => navigate('/courses-list')}
-            />
+    <>
+      <div className="position-relative md:px-36 px-8 pt-20 text-left">
+        <div className="flex items-start md:flex-row flex-col w-full justify-between">
+          <div>
+            <h1 className="text-4xl font-semibold text text-gray-800">
+              CoursesList
+            </h1>
+            <p className="text-gray-500">
+              <span
+                className="text-blue-600 cursor-pointer"
+                onClick={() => navigate('/')}
+              >
+                Home
+              </span>{' '}
+              / <span>CoursesList</span>
+            </p>
           </div>
-        )
-      }
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-14">
-        {filteredCourses.map((course, index) => (
-          <CourseCard key={index} course={course} />
-        ))}
+          <SearchBar data={input} />
+        </div>
+        {
+          // conditionally render filter text
+          input && (
+            <div className="inline-flex items-center gap-4 px-4 py-2 bg-gray-100 rounded-full">
+              <p>{input}</p>
+              <img
+                src={assets.cross_icon}
+                alt="cursor"
+                className="cursor-pointer "
+                onClick={() => navigate('/courses-list')}
+              />
+            </div>
+          )
+        }
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-14">
+          {filteredCourses.map((course, index) => (
+            <CourseCard key={index} course={course} />
+          ))}
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
